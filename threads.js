f6589bf15b5aac3f463df63065a18bfba3801c6d
jdiperi88 <jdiperi88@yahoo.com>
2018-10-04 10:10:10 -0400

    added multiple function calls to demonstrate using of libuvs to multithread

--- threads.js
+++ threads.js
@@ -1,3 +1,5 @@
+// accessing env thread pool var and setting it to 2
+process.env.UV_THREADPOOL_SIZE = 4;
 const crypto = require("crypto");
 
 const start = Date.now();
@@ -7,4 +9,22 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
 });
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
 	console.log(`2: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`3: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`4: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`5: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`6: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`7: ${Date.now() - start}`);
+});
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+	console.log(`8: ${Date.now() - start}`);
 });
