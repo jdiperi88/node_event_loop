8fb5c6422d57b0b489490713df29caac3d4b7b3b
jdiperi88 <jdiperi88@yahoo.com>
2018-10-04 12:01:03 -0400

    setup basic structure for clustering and mini express app to test clustering

---
+++ index.js
@@ -0,0 +1,23 @@
+const cluster = require("cluster");
+
+// is the file being executed in master mode
+if (cluster.isMaster) {
+	// cause index.js to be executed *again* but in child mode.
+	cluster.fork();
+} else {
+	// I'm a child, im going to act like a server and do nothing else
+	const express = require("express");
+	const app = express();
+
+	function doWork(duration) {
+		const start = Date.now();
+		while (Date.now() - start < duration) {}
+	}
+
+	app.get("/", (req, res) => {
+		doWork(5000);
+		res.send("hi there");
+	});
+
+	app.listen(3000);
+}
